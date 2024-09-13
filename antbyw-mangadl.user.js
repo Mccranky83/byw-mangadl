f47a070182cc4115b06708c82cfbeecf891ac0a5
Mccranky83 <ericcai20021118@gmail.com>
2024-09-13 23:09:20 +0800

    Prepare for download
    Fetch image info from each chapter page

--- antbyw-mangadl.user.js
+++ antbyw-mangadl.user.js
@@ -4,118 +4,250 @@
 // @version      2024-09-10
 // @description  下载单行本漫画
 // @author       mccranky
-// @match        https://www.antbyw.com/plugin.php?id=jameson_manhua&c=index&a=bofang&kuid=136952
+// @match        http://*/plugin.php?id=jameson_manhua*a=bofang*kuid*
+// @match        http://*/plugin.php?id=jameson_manhua*kuid*a=bofang*
+// @match        https://*/plugin.php?id=jameson_manhua*a=bofang*kuid*
+// @match        https://*/plugin.php?id=jameson_manhua*kuid*a=bofang*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=antbyw.com
 // @grant        none
 // ==/UserScript==
 
 "use strict";
 
-window.addEventListener("load", () => {
-  const script = document.createElement("script");
-  script.src =
-    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js";
-  (document.head || document.documentElement).appendChild(script);
-  script.onload = () => {
-    class MangaDl {
-      constructor() {
-        this.manga_name = this.getChapterInfo().manga_name;
-        this.chap_list = this.getChapterInfo().chap_list;
-      }
-
-      getChapterInfo() {
-        const title = $("title").text();
-        const manga_name_jp = title.match(/(?<=【)[^[【】]+(?=】)/g)[1];
-        const manga_name_zh = title.split("【")[0];
-        const manga_name = manga_name_zh + " / " + manga_name_jp;
-        let chap_list = [];
-        $(".uk-container ul.uk-switcher .muludiv a.zj-container").each(
-          (_, cur) => {
-            const url = [
-              window.location.protocol,
-              "//",
-              window.location.host,
-              $(cur).attr("href"),
-            ].join("");
-            chap_list.push({
-              number: $(cur).text(),
-              url,
-            });
-          },
-        );
-        return { chap_list, manga_name };
-      }
-    }
-
-    const mangadl = new MangaDl();
-    // Create menu
-    (() => {
-      let entry_chap = [],
-        end_chap = [];
-      mangadl.chap_list.forEach((cur, i) => {
-        entry_chap.push(
-          `<option value="${i}" ${i ? "" : "selected"}>
-            <span>${cur.number}</span>
-          </option>`,
-        );
-        end_chap.push(
-          `<option value="${i}" ${i === mangadl.chap_list.length - 1 ? "selected" : ""}>
-            <span>${cur.number}</span>
-          </option>`,
-        );
-      });
-      entry_chap.join("\n");
-      end_chap.join("\n");
-      const menu_html = `
-        <div id="injected">
-          <span>開始：</span>
-          <select name="entry" class="uk-select">${entry_chap}</select>
-          <span>結束：</span>
-          <select name="end" class="uk-select">${end_chap}</select>
-          <br />
-          <div class="mtm">
-            <a href="javascript:;" class="uk-button uk-button-danger" id="mangadl-all">
-              <span>打包下載</span>
-            </a>
-            <a href="javascript:;" class="uk-button uk-button-primary" id="mangadl-retry">
-              <span>重新下載</span>
-            </a>
-            <a href="javascript:;" class="uk-button uk-button-primary" id="mangadl-seperate">
-              <span>分批下載</span>
-            </a>
-          </div>
-        </div>
-      `;
-      $("div.uk-width-expand .uk-margin-left").append(menu_html);
-      $("#mangadl-all").on("click", startDownload);
-
-      // Add styles
+window.addEventListener("load", async () => {
+  const scripts = [
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js",
+  ];
+  await Promise.all(
+    scripts.map(
+      (cur) =>
+        new Promise((res, rej) => {
+          const script = document.createElement("script");
+          script.src = cur;
+          script.onload = res;
+          script.onerror = rej;
+          (document.head || document.documentElement).appendChild(script);
+        }),
+    ),
+  )
+    .then(() => {
+      console.log("scripts successfully loaded...");
       (() => {
-        const css = `
-        #injected span {
-          padding: 2px 8px;
-          display: inline;
+        class MangaDl {
+          constructor() {
+            this.chap_info = this.getChapterInfo();
+            this.manga_name = this.chap_info.manga_name;
+            this.chap_list = this.chap_info.chap_list;
+            this.chap_dllist = [];
+            this.entry_chap = 0;
+            this.end_chap = 0;
+            this.dling = false;
+            this.zip = {};
+          }
+
+          init() {
+            this.dling = false;
+            this.entry_chap = 0;
+            this.end_chap = 0;
+            this.zip = new JSZip();
+          }
+          getChapterInfo() {
+            const title = $("title").text();
+            let manga_name_jp = "";
+            let manga_name_zh = "";
+            if (title.includes("【")) {
+              manga_name_jp = title.match(/(?<=【)[^[【】]+(?=】)/g)[1];
+              manga_name_zh = title.split("【")[0];
+            } else {
+              manga_name_zh = title.split(" ")[0];
+            }
+            const manga_name =
+              manga_name_zh + manga_name_jp
+                ? "｜" + manga_name_jp
+                : manga_name_jp;
+            let chap_list = [];
+            const push_chap = (selector) => {
+              $(selector).each((_, cur) => {
+                const url = [
+                  window.location.protocol,
+                  "//",
+                  window.location.host,
+                  "/",
+                  $(cur).attr("href"),
+                ].join("");
+                chap_list.push({
+                  number: $(cur).text().padStart(2, "0"),
+                  url,
+                });
+              });
+            };
+            let selector = "";
+            switch (location.hostname.includes("ant")) {
+              case true:
+                selector =
+                  ".uk-container ul.uk-switcher .muludiv a.uk-button-default";
+                break;
+              case false:
+                selector = ".uk-grid-collapse .muludiv a";
+                break;
+              default:
+                break;
+            }
+            push_chap(selector);
+            return { chap_list, manga_name };
+          }
         }
 
-        #injected select {
-          width: 60px;
-          height: 30px;
-          line-height: 30px;
-          border-radius: 2px;
-        }
+        const mangadl = new MangaDl();
+        // Creating menu
+        (() => {
+          let entry = [],
+            end = [];
+          mangadl.chap_list.forEach((cur, i) => {
+            entry.push(`
+              <option value="${i}" ${i ? "" : "selected"}>
+                ${cur.number}
+              </option>`);
+            end.push(`
+              <option value="${i}" ${i === mangadl.chap_list.length - 1 ? "selected" : ""}>
+                ${cur.number}
+              </option>`);
+          });
+          entry.join("\n");
+          end.join("\n");
+          const menu_html = `
+            <div id="injected">
+              <span>開始：</span>
+              <select name="entry" class="uk-select">${entry}</select>
+              <span>結束：</span>
+              <select name="end" class="uk-select">${end}</select>
+              <br />
+              <div class="mtm">
+                <a href="javascript:;" class="uk-button uk-button-danger" id="mangadl-all">
+                  <span>打包下載</span>
+                </a>
+                <a href="javascript:;" class="uk-button uk-button-primary none" id="mangadl-retry">
+                  <span>重新下載</span>
+                </a>
+                <a href="javascript:;" class="uk-button uk-button-primary" id="mangadl-seperate">
+                  <span>分批下載</span>
+                </a>
+              </div>
+            </div>
       `;
-        const style = $("<style>", { type: "text/css" }).html(css);
-        $(document.head || document.documentElement).append(style);
-      })();
-    })();
+          $("div.uk-width-expand .uk-margin-left").append(menu_html);
+          $("#mangadl-all").on("click", loadInfo);
 
-    function startDownload() {
-      if ($("#mangadl-all").hasClass("uk-dling")) {
-        $("#mangadl-all").text("下載中稍等..");
-        return;
-      } else {
-        $("#mangadl-all").addClass("uk-dling").text("下載中");
-      }
-    }
-  };
+          // Adding css styles
+          (() => {
+            const css = `
+              #injected span {
+                padding: 2px 8px;
+                display: inline;
+              }
+
+              #injected select {
+                width: 80px;
+                height: 30px;
+                line-height: 30px;
+                border-radius: 2px;
+              }
+            `;
+            const style = $("<style>", { type: "text/css" }).html(css);
+            $(document.head || document.documentElement).append(style);
+          })();
+        })();
+
+        function loadInfo() {
+          if ($("#mangadl-all").attr("dling")) {
+            $("#mangadl-all").text("下載中稍等..");
+            return;
+          } else {
+            mangadl.init();
+            mangadl.dling = true;
+            $("#mangadl-all").attr("dling", mangadl.dling).text("下載中");
+          }
+
+          mangadl.entry_chap = Number($("#injected [name='entry']").val());
+          mangadl.end_chap = Number($("#injected [name='end']").val());
+
+          if (mangadl.entry_chap > mangadl.end_chap) {
+            [mangadl.entry_chap, mangadl.end_chap] = [
+              mangadl.end_chap,
+              mangadl.entry_chap,
+            ];
+          }
+
+          mangadl.chap_dllist = mangadl.chap_list.slice(
+            mangadl.entry_chap,
+            mangadl.end_chap + 1,
+          );
+
+          mangadl.chap_dllist.forEach((cur) => getImgList(cur));
+        }
+
+        async function getImgList(chap) {
+          const dirname = `${mangadl.manga_name}/${chap.number}`;
+          const folder = mangadl.zip.folder(dirname);
+          fetchT(chap.url, { method: "GET" }, 10_000)
+            .then((res) => {
+              if (!res.ok) throw new Error("chapter request failed...");
+              else console.log("chapter request successful...");
+              return res.text();
+            })
+            .then((res) => {
+              const $nodes = $(
+                new DOMParser().parseFromString(res, "text/html").body,
+              );
+              if (!$nodes.find(".wp").length) {
+                console.error("failed to load chapter...");
+                setTimeout(() => {
+                  getImgList(chap);
+                  return;
+                });
+              } else if (!$nodes.find(".jameson_manhua").length) {
+                const filename = "權限不足.txt";
+                folder.file(filename, "權限不足，請登錄賬戶或使用VIP帳戶！\n");
+                genMsgFile(filename);
+              } else if (!$nodes.find(".uk-zjimg img").length) {
+                const filename = "VIP專屬.txt";
+                folder.file(filename, "請使用VIP帳戶下載！\n");
+                genMsgFile(filename);
+              } else {
+                $nodes.find(".uk-zjimg img").each(function () {
+                  dlImg($(this).src, chap.number);
+                });
+              }
+              function genMsgFile(filename) {
+                mangadl.zip
+                  .file(dirname + "/" + filename)
+                  .async("string")
+                  .then((data) => console.error(data));
+              }
+            })
+            .catch((e) => {
+              console.error(e.message);
+            });
+        }
+
+        function dlImg() {
+          console.log("TEST_MSG: FUCK U!");
+        }
+
+        function fetchT(url, options, timeout) {
+          const c = new AbortController();
+          const signal = c.signal;
+          const fetch_p = fetch(url, { ...options, signal });
+          const timeout_p = new Promise((_, rej) => {
+            setTimeout(() => {
+              c.abort();
+              rej(new Error("request timeout..."));
+            }, timeout);
+          });
+          return Promise.race([fetch_p, timeout_p]);
+        }
+      })();
+    })
+    .catch(() => console.error("scripts failed to load..."));
 });
