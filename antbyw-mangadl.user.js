d215fc6db232766b9964c94643bfa6c94e356ad1
Mccranky83 <ericcai20021118@gmail.com>
2024-09-24 23:50:06 +0800

    Refactor event listeners for better navigation
    Remove nested event listeners and assign keybinds to existing ones to
    improve navigation.

--- antbyw-mangadl.user.js
+++ antbyw-mangadl.user.js
@@ -197,6 +197,25 @@ window.addEventListener("load", async () => {
           const $sidebarBtn = $("#sidebar-open-btn");
           const $closeBtn = $("#sidebar-close-btn");
 
+          $(document).on("keydown", ({ key, ctrlKey }) => {
+            if (key === "o" && ctrlKey) {
+              $sidebar.addClass("active");
+              $sidebarBtn.addClass("hidden");
+            }
+          });
+
+          $(document).on("keydown", ({ key, ctrlKey }) => {
+            if (key === "q" && ctrlKey) {
+              $sidebar.removeClass("active");
+              $sidebarBtn.removeClass("hidden");
+            }
+          });
+
+          $closeBtn.on("click", () => {
+            $sidebar.removeClass("active");
+            $sidebarBtn.removeClass("hidden");
+          });
+
           $sidebarBtn.on("mousedown", ({ clientX, clientY }) => {
             // Calculate offset between mouse and element border
             const btn_rect = $sidebarBtn[0].getBoundingClientRect();
@@ -204,12 +223,6 @@ window.addEventListener("load", async () => {
             const shiftY = clientY - btn_rect.top;
 
             $(document).on("mousemove", onMouseMove);
-            $(document).on("keydown", ({ key, ctrlKey }) => {
-              if (key === "q" && ctrlKey) {
-                $sidebar.removeClass("active");
-                $sidebarBtn.removeClass("hidden");
-              }
-            });
             $sidebarBtn.on("mouseup", () => {
               if (!dragging) {
                 $sidebar.addClass("active");
@@ -219,10 +232,6 @@ window.addEventListener("load", async () => {
               $sidebarBtn.off("mouseup");
             });
 
-            $closeBtn.on("click", () => {
-              $sidebar.removeClass("active");
-              $sidebarBtn.removeClass("hidden");
-            });
             /**
              * pageX/Y returns the absolute position
              * However, $sidebarBtn's position is fixed
