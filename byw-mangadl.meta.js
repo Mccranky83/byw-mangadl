451a60513115cd75e5f533738983a77f722980cd
Mccranky83 <ericcai20021118@gmail.com>
2024-10-10 17:51:26 +0800

    Rename userscript

--- byw-mangadl.meta.js
+++ byw-mangadl.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         antbyw下載
+// @name         byw漫畫下載
 // @version      1.0
 // @namespace    mccranky83.github.io
 // @description  下載搬運網單行本漫畫
@@ -11,8 +11,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=antbyw.com
 // @grant        GM_xmlhttpRequest
 // @connect      zerobywz.com
-// @connect      antbyw.com
-// @updateURL    https://github.com/Mccranky83/byw-mangadl/raw/main/antbyw-mangadl.meta.js
-// @downloadURL  https://github.com/Mccranky83/byw-mangadl/raw/main/antbyw-mangadl.user.js
+// @updateURL    https://github.com/Mccranky83/byw-mangadl/raw/main/byw-mangadl.meta.js
+// @downloadURL  https://github.com/Mccranky83/byw-mangadl/raw/main/byw-mangadl.user.js
 // @license      MIT
 // ==/UserScript==
